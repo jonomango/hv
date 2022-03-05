91fedc26267f7550385bc7e84646e562b1fb30d1
jono <26609800+jonomango@users.noreply.github.com>
2022-03-05 14:31:26 -0800

    read_phys_mem hypercall is implemented more correctly now (used to only work for buffers that were mapping in both guest and host address-space). removed write_phys_mem until read_phys_mem kinks are worked out.

--- hv/hypercalls.cpp
+++ hv/hypercalls.cpp
@@ -6,6 +6,63 @@
 
 namespace hv::hc {
 
+// GVA -> HVA
+// TODO: account for large pages
+// TODO: account for page boundaries (could be done by
+//       mapping guest pages into HV address-space?)
+static void* translate_guest_address(void* const address) {
+  cr3 guest_cr3;
+  guest_cr3.flags = vmx_vmread(VMCS_GUEST_CR3);
+
+  // 4-level virtual address
+  union virtual_address {
+    void const* address;
+    struct {
+      uint64_t offset   : 12;
+      uint64_t pt_idx   : 9;
+      uint64_t pd_idx   : 9;
+      uint64_t pdpt_idx : 9;
+      uint64_t pml4_idx : 9;
+    };
+  };
+
+  virtual_address const vaddr = { address };
+
+  // guest PML4
+  auto const pml4 = reinterpret_cast<pml4e_64*>(host_physical_memory_base
+    + (guest_cr3.address_of_page_directory << 12));
+  auto const pml4e = pml4[vaddr.pml4_idx];
+
+  if (!pml4e.present)
+    return nullptr;
+
+  // guest PDPT
+  auto const pdpt = reinterpret_cast<pdpte_64*>(host_physical_memory_base
+    + (pml4e.page_frame_number << 12));
+  auto const pdpte = pdpt[vaddr.pdpt_idx];
+
+  if (!pdpte.present)
+    return nullptr;
+
+  // guest PD
+  auto const pd = reinterpret_cast<pde_64*>(host_physical_memory_base
+    + (pdpte.page_frame_number << 12));
+  auto const pde = pd[vaddr.pd_idx];
+
+  if (!pde.present)
+    return nullptr;
+
+  // guest PT
+  auto const pt = reinterpret_cast<pte_64*>(host_physical_memory_base
+    + (pde.page_frame_number << 12));
+  auto const pte = pt[vaddr.pt_idx];
+
+  if (!pte.present)
+    return nullptr;
+
+  return host_physical_memory_base + (pte.page_frame_number << 12) + vaddr.offset;
+}
+
 // ping the hypervisor to make sure it is running
 void ping(vcpu* const cpu) {
   cpu->ctx->rax = hypervisor_signature;
@@ -29,17 +86,19 @@ void read_phys_mem(vcpu* const cpu) {
   // size in bytes
   auto const size = cpu->ctx->r9;
 
+  auto const hva = translate_guest_address(dst);
+
+  if (!hva) {
+    // TODO: should be a #PF instead
+    inject_hw_exception(general_protection, 0);
+    return;
+  }
+
   host_exception_info e;
-  memcpy_safe(e, dst, host_physical_memory_base + src, size);
+  memcpy_safe(e, hva, host_physical_memory_base + src, size);
 
+  // better safe than sorry
   if (e.exception_occurred) {
-    if (e.vector == page_fault) {
-      // TODO: reflect #PF into guest
-      inject_hw_exception(general_protection, 0);
-      return;
-    }
-
-    // something bad happened :(
     inject_hw_exception(general_protection, 0);
     return;
   }
@@ -48,32 +107,8 @@ void read_phys_mem(vcpu* const cpu) {
 }
 
 // write to arbitrary physical memory
-void write_phys_mem(vcpu* const cpu) {
-  // physical address
-  auto const dst = cpu->ctx->rdx;
-
-  // virtual address
-  auto const src = reinterpret_cast<void*>(cpu->ctx->r8);
-
-  // size in bytes
-  auto const size = cpu->ctx->r9;
-
-  host_exception_info e;
-  memcpy_safe(e, host_physical_memory_base + dst, src, size);
-
-  if (e.exception_occurred) {
-    if (e.vector == page_fault) {
-      // TODO: reflect #PF into guest
-      inject_hw_exception(general_protection, 0);
-      return;
-    }
-
-    // something bad happened :(
-    inject_hw_exception(general_protection, 0);
-    return;
-  }
-
-  skip_instruction();
+void write_phys_mem(vcpu* const) {
+  inject_hw_exception(invalid_opcode);
 }
 
 
